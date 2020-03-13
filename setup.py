8562ebd54f376f996f88838dfa60302442a4183e
Nicolas Besson <nicolas@iscab.fr>
2020-03-13 13:55:34 +0100

    Increase revision number to 0.4.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 setup(
     name = "pyRpc",
     version = ctx['__version__'],
-    url = 'https://github.com/justinfx/pyRpc',
+    url = 'https://github.com/iscab-fr/pyRpc',
     packages = find_packages(),
     include_package_data = True,
     install_requires = ['pyzmq'],
